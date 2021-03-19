a347031a91f8e62e3b651fa8fc19101f263ce97b
Jamahl Carter <jamahl.carter@enett.com>
2021-03-20 07:22:36 +1100

    Added basic UI workflows, added prod dockerfile

--- CoinPrice.Ui/src/App.js
+++ CoinPrice.Ui/src/App.js
@@ -1,39 +1,117 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
+  Select,
+  Button,
+  FormLabel,
+  Input,
+  Heading,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { Formik } from 'formik';
 
 function App() {
+  // Initilise state
+  const [state, setCoinDetails] = useState({
+    CoinType: 'BTC',
+    Ask: 0.0,
+    PercentageChanged: '0%'
+  });
+
+  // Handler to refresh price for preferred coin
+  const refreshHandler = async () => {
+    // Pull price data for preferred coin
+    const response = await fetch(
+      `https://coin-price-api.azurewebsites.net/api/price`
+    ).then(resp => resp.json());
+
+    // Calculate percentage changed
+    const percentageChanged =
+      state.Ask === 0 ? 0 : ((response.ask - state.Ask) / state.Ask) * 100;
+
+    // Update State
+    const result = {
+      CoinType: response.coinType,
+      Ask: response.ask,
+      PercentageChanged: `${percentageChanged}%`,
+    };
+    setCoinDetails(result);
+
+    return result;
+  };
+
+  // Handler to set user preferred coin
+  const setPreferredHandler = async values => {
+    const options = {
+      method: 'PUT',
+    };
+    await fetch(
+      `https://coin-price-api.azurewebsites.net/api/user/${values.CoinType}`,
+      options
+    );
+    setCoinDetails({...state, ...{PercentageChanged: '0%'}}) // changing preference reset %
+  };
+
+  // Populate form on load
+  useEffect(() => {
+    refreshHandler();
+  }, []);
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
+      <Box maxW="800px" w="100%" mx="auto">
+        <center>
+          <Heading>Coin Price Application</Heading>
+        </center>
+        {/* Set preferred coin form */}
+        <Formik
+          initialValues={{ ...state }}
+          onSubmit={(values, { setSubmitting }) => {
+            setPreferredHandler(values).then(_ => setSubmitting(false));
+          }}
+          enableReinitialize
+        >
+          {({ values, handleChange, handleSubmit, isSubmitting }) => (
+            <form onSubmit={handleSubmit}>
+              <FormLabel style={{ display: 'block' }}>Select Coin</FormLabel>
+              <Select
+                name="CoinType"
+                value={values.CoinType}
+                onChange={handleChange}
+                style={{ display: 'block' }}
+              >
+                <option value="BTC">BTC</option>
+                <option value="ETH">ETH</option>
+                <option value="XRP">XRP</option>
+              </Select>
+              <Button type="submit" isLoading={isSubmitting} colorScheme="teal">
+                Set preference
+              </Button>
+            </form>
+          )}
+        </Formik>
+        {/* Refresh details form */} {/*TODO - MOVE TO SEPARATE COMPOENT*/}
+        <Formik
+          initialValues={{ ...state }}
+          onSubmit={(values, { setSubmitting }) => {
+            refreshHandler().then(_ => {
+              setSubmitting(false);
+            });
+          }}
+          enableReinitialize
+        >
+          {({ values, handleChange, handleSubmit, isSubmitting }) => (
+            <form onSubmit={handleSubmit}>
+              <FormLabel>AskValue:</FormLabel>
+              <Input readOnly value={values.Ask}></Input>
+              <FormLabel>Percentage Changed:</FormLabel>
+              <Input readOnly value={values.PercentageChanged}></Input>
+              <Button type="submit" isLoading={isSubmitting} colorScheme="teal">
+                Refresh
+              </Button>
+            </form>
+          )}
+        </Formik>
       </Box>
     </ChakraProvider>
   );
